905e2b568b09144d33646908aa326bc6200019fe
Long Hong Vo <honglongvo@gmail.com>
2024-09-23 13:40:16 +0700

    Mô tả về thay đổi

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+const http = require("http");
+const express = require("express");
+
+const hostname = 'localhost'; // localhost
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text-plain');
+    res.end('Hello Long');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
